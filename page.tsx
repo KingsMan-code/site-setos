26e6b96aedd6a4ec3ce4206fab29ca46b57f6ccc
Matheus Figueiredo <matheus.figueiredo@br.ey.com>
2025-07-14 15:44:49 -0300

    first commit

---
+++ page.tsx
@@ -0,0 +1,10 @@
+import { Flex, Text, Button } from "@radix-ui/themes";
+
+export default function Page() {
+  return (
+    <Flex direction="column" gap="2">
+      <Text>Olá com Radix Themes!</Text>
+      <Button>Vamos lá</Button>
+    </Flex>
+  );
+}
